21d9fe117563199bdccd9b3227bf11574e46bcc6
cycawy <cycawy@gmail.com>
2021-03-06 00:29:16 -0800

    update with adding pictures

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ImgOne from './img/img-1.jpeg';
 
 function App() {
   return (
     <div className="App">
       <h1>My new APP</h1>
+      <p>this is a test for re-deployment</p>
+      <img src={ImgOne} />
     </div>
   );
 }
