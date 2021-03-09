ef06a1f8b203779fe07bcec9c595e2fd3e65f95a
cycawy <cycawy@gmail.com>
2021-03-08 23:55:48 -0800

    add more picture content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import ImgOne from './img/img-1.jpeg';
+import ImgTwo from './img/img-2.jpeg';
 
 function App() {
   return (
     <div className="App">
       <h1>My new APP</h1>
       <p>this is a test for re-deployment</p>
-      <img src={ImgOne} />
+      <div className='flex flex-column bg-black text-white'>
+        <p className='bg-black text-white'>dilicious food</p>
+        <img src={ImgOne} />
+      </div>
+      
+      <p>this is a test for re-deployment</p>
+      <div>
+        <img src={ImgTwo} />
+      </div>
+      
     </div>
   );
 }
